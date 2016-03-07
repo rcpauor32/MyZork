13e5b772ae5876be90276d995acbed70e21ffa49
rcpauor32 <rcolivepau@gmail.com>
2016-03-07 23:04:53 +0100

    Created "world.h" w/ players, exits and rooms classes

--- Zork/main.cpp
+++ Zork/main.cpp
@@ -1,14 +1,6 @@
-#include "header.h"
+
 
 int Main() {
-	Start();
-	
-	while (g.player_input) {
-		Draw();
 
-		scanf_s("%c ", &g.player_input, 1);
-
-		Move();
-	}
 
 }
--- Zork/world.h
+++ Zork/world.h
@@ -1,8 +1,42 @@
-#include "header.h"
 
-struct globals {
-	char player_input;
-	Position player_pos;
-	Room Secret_R, Surgery_R, Studio, UpAR, DwAR, Stone_R, Library, Mirror_R, Waiting_R, Dark_R, Reception;
-}g;
+class players {
+public:
+	int x;
+	int y;
+};
+
+class rooms {
+public:
+	int x;
+	int y;
+	char* name;
+};
+
+class exits {
+public:
+	int x;
+	int y;
+	char* direction[6];
+};
+
+class world {
+public:
+	players* player = new players;
+	rooms* room = new rooms[];
+	exits* exit = new exits[];
+
+	enum {
+		Secret_R,
+		Surgery,
+		Studio,
+		UpAR,
+		DwnAR,
+		Stone_R,
+		Mirror_R,
+		Library,
+		Dark_R,
+		Waiting_R,
+		Reception
+	};
+	}
 
